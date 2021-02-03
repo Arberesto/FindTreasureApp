43887bad72f9e28368ff87d0dd127b9d348e8df5
Bagaev Dmitriy <devilprime5@gmail.com>
2021-02-04 05:16:48 +0600

    [*] add conditions to display one alert per time; [*] try to implement issue count

--- src/internalMagic/InternalMagic.js
+++ src/internalMagic/InternalMagic.js
@@ -1,67 +1,105 @@
+export function _3words () {
+    alert('Так дайте металлоискатель')
+};
 
-let errorsFixed =  new Array(5);
-for (let i = 0; i < 5; i++) {
-    errorsFixed[i] = false
+//function call our API to get the treasure
+
+export function findTheTreasure (url, destination) {
+    let junk = callAPI(url, destination)
+};
+
+function condAlert(text, isAlerted) {
+    if (! isAlerted) {
+        alert(text);
+    }
 }
+
+window.result = 0;
+let errorFixed = new Array(5);
+for (let i = 0; i < 5; i++) {
+    errorFixed[i] = false
+}
+
+window.errorFixed = errorFixed;
+
 
 export function callAPI (url, destination) {
 
+    let isAlerted = false;
+    let result = 0;
+
     // 1 error
 
-    if ( ! url.includes('https') ) {
-        errorsFixed[1]=true;
+    if (! (url.toString().includes('https'))) {
+        window.errorFixed[1]=true;
+        result +=1;
     } else {
-        errorsFixed[2]=false;
-        //alert('Здесь все слишком охраняется, нужно менее безопасное место');
+        window.errorFixed[2]=false;
+        condAlert('Здесь все слишком охраняется, нужно менее безопасное место', isAlerted)
+        isAlerted = true;
     }
 
     // 2 error
+    let buttons = document.getElementsByClassName('main__button-answer');
+    //alert(buttons.length)
+    let button = buttons[0];
+    //alert(button.onclick);
 
-    /*
-    if (document.getElementById('button').onclick != '_3words') {
-        errorsFixed[2]=true
+    if (! button.onclick.toString().includes("_3words")) {
+        window.errorFixed[2]=true;
+        result +=1;
     } else {
-        errorsFixed[2]=false
+        window.errorFixed[2]=false;
+        isAlerted = true;
     }
-     */
 
-    alert (document.title);
+    //alert (document.title);
 
     // 3 error
-
-    if (document.title != 'hat') {
-        errorsFixed[3]=true
+    if (! document.title.toString().includes("hat")) {
+        window.errorFixed[3]=true;
+        result +=1;
     } else {
-        errorsFixed[3]=false
-        //alert(‘Зачем мне сокровища, если есть роскошная шляпа?’)
+        window.errorFixed[3]=false;
+        condAlert('Зачем мне сокровища, если есть роскошная шляпа?', isAlerted);
+        isAlerted = true;
     }
 
     // 4 error
-
-    if (! url.includes('.com')) {
-        errorsFixed[4]=true
+    if (url.includes('.com')) {
+        window.errorFixed[4]=false;
+        condAlert('Каждый из наш в душе - настоящий пират, а не просто сотрудник компании', isAlerted);
+        isAlerted = true;
     } else {
-        errorsFixed[4]=false
-        //alert('Каждый из наш в душе - настоящий пират, а не просто сотрудник компании')
+        window.errorFixed[4]=true;
+        result +=1;
     }
 
 
     // 5 error
 
-    if (! url === 's://w') {
-        errorsFixed[5]=true
+    if (! destination.includes("://w")) {
+        window.errorFixed[5]=true;
+        result +=1;
     } else {
-        errorsFixed[5]=false
-        //alert('Я вижу мусор, сначало нужно прибраться')
+        window.errorFixed[5]=false;
+        condAlert('Я вижу мусор, сначало нужно прибраться', isAlerted);
+        isAlerted = true;
     }
 
+    condAlert('Seems like you have all pieces of my map, enjoy', isAlerted)
 
+
+    window.result = result;
+
+    return result;
 }
 
 export function getSolvedIssuesNumber() {
     let result = 0;
     for (let i = 0; i < 5; i++) {
-        if (errorsFixed[i]) {
+        //alert(window.errorFixed[i]);
+        if (window.errorFixed[i]) {
             result +=1;
         }
     }
--- src/mainPage/MainPage.js
+++ src/mainPage/MainPage.js
@@ -1,34 +1,32 @@
 import React from 'react';
 import ProgressBar from "../progressBar/ProgressBar";
-import {callAPI, getSolvedIssuesNumber} from "../internalMagic/InternalMagic";
+import {getSolvedIssuesNumber,_3words, findTheTreasure} from "../internalMagic/InternalMagic";
 import './style.css';
 
 export default class MainPage extends React.Component {
-
-    _3words = () => {
-        alert('Так дайте металлоискатель')
-    };
-
-    findTheTreasure = (url, destination) => {
-        callAPI(url, destination)
-    };
-
-    buttonOnClick = () => {
-        let url='https://api.findtreasure.com'
-        let junk='://w'
-        this.findTheTreasure(url, junk);
+    constructor(props) {
+        super(props);
+        this.buttonOnClick = this.buttonOnClick.bind(this);
+        this.state = {
+            issuesFixedNumber: 1
+        }
     }
+    buttonOnClick() {
+        let url="http://api.findtreasure.pirate";
+        let junk=':s//w';
 
-    getSolvedIssuesNumber = () => {
-        return 1;
+        //call the api to find treasure
+
+        findTheTreasure(url, junk);
     }
 
     render() {
+    const fixedIssueNumber = getSolvedIssuesNumber();
     return (
       <div className={'main'}>
         <p className={'main__text'}>text</p>
-        <button className={'main__button-answer button'} onClick= { () => {this.buttonOnClick()}} >Find the treasure!</button>
-        <ProgressBar currentValue= {getSolvedIssuesNumber()} maxValue={5}/>
+        <button className={'main__button-answer button'} onClick = { this.buttonOnClick} >Find the treasure!</button>
+        <ProgressBar currentValue= {fixedIssueNumber} maxValue={5}/>
         <button className={'main__button-hint button'} >Give me a hint</button>
       </div>
     );
