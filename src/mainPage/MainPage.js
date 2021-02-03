8607a9e92ed8627dd26b3ba786471757545af19c
Bagaev Dmitriy <devilprime5@gmail.com>
2021-02-04 02:43:54 +0600

    [*] fix ProgressBar import; add alert on findTreasure button

--- src/mainPage/MainPage.js
+++ src/mainPage/MainPage.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import ProgressBar from "../progressBar/MainPage";
+import ProgressBar from "../progressBar/ProgressBar";
 import './style.css';
 
 export default class MainPage extends React.Component {
 
-  render() {
+
+    render() {
     return (
       <div className={'main'}>
         <p className={'main__text'}>text</p>
-        <button className={'main__button-answer button'}>Find the treasure!</button>
+        <button className={'main__button-answer button'} onClick= {() => {alert('hooray')}}>Find the treasure!</button>
         <ProgressBar currentValue={4} maxValue={10}/>
-        <button className={'main__button-hint button'}>Give me a hint</button>
+        <button className={'main__button-hint button'} >Give me a hint</button>
       </div>
     );
   };
